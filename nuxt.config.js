42e542725e67cf6239f546f905cd30878ce26414
Andrei Stefanie <andrei.stefanie@gmail.com>
2018-04-08 23:12:02 +0300

    Added favicon and title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'nuxt',
+    title: 'Andrei Stefanie Portfolio',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
