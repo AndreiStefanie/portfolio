dc54f4617f8739fc8dde15f409e0c85a01daab81
Andrei Stefanie <andrei.stefanie@gmail.com>
2018-08-19 10:39:34 +0300

    Added about me

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="container pt-4 pb-4">
-    <h1>About Me</h1>
+    <b-card title="About me"><about/></b-card>
   </div>
 </template>
 
 <script>
+import About from '~/components/About'
+
 export default {
-  
+  name: 'AboutPage',
+  components: {
+    About
+  }
 }
 </script>
 
